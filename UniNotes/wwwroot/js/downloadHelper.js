4dab39d885587722bb1e365e1a47906e47dd270c
daglaroglou <christos.daglaroglou@gmail.com>
2025-05-01 06:02:11 +0300

    Add full-screen note viewer

---
+++ UniNotes/wwwroot/js/downloadHelper.js
@@ -0,0 +1,8 @@
+function downloadFromBase64(base64Data, contentType, fileName) {
+    var link = document.createElement('a');
+    link.href = `data:${contentType};base64,${base64Data}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
